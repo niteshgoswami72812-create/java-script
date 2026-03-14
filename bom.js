ecdf7e35136e02cb0c963d5e43c941a135167838
niteshgoswami72812-create <niteshgoswami72812@gmail.com>
2026-03-14 15:45:56 +0530

    nitesh

---
+++ bom.js
@@ -0,0 +1,163 @@
+
+function nitesh(){
+
+    var myname = "Nitesh"
+    
+    console.log(myname)
+}
+
+nitesh()
+
+function Anurag(){
+
+    console.log("Anurag")
+}
+
+Anurag()
+
+function rahul(){
+
+    console.log(11*5)
+}
+
+rahul()
+
+function rohit(){
+
+    console.log(99-11)
+}
+
+rohit()
+
+function rahulrohit () {
+
+    var a = 9
+    var b = 10
+    console.log(a+b)
+}
+
+rahulrohit ()
+
+function number (){
+
+    //type coercion
+    //first priority
+    // second priority numbers
+    // true value 1, false value 0
+    var a = 5
+    var b = null
+    console.log(a+b);
+}
+number()
+
+// function bom(){
+
+//     // alert()
+//     var a = 4
+//     alert(a)
+// }
+// bom()
+
+//  function bom(){
+
+//     //prompt()
+//     var myname = prompt("Enter your name")
+//     alert("welcome" + myname)
+    
+// }
+// bom()
+
+// function bom() {
+//     var num1 = prompt("Enter first number");
+//     var num2 = prompt("Enter second number");
+
+//     var sum = Number(num1) + Number(num2);
+
+//     alert(sum);
+//     alert("welcome");
+// }
+
+// bom();
+
+// function bom(){
+//     var num1 = prompt("Enter first number");
+//     var num2 = prompt("Enter second number");
+
+//     var sum = Number(num1) + Number(num2);
+
+//     alert(sum);
+//     alert("welocme");
+// }
+
+// bom()
+
+// function bom(){
+
+//  var age = prompt("Enter your age")
+ 
+//  if( age >= 21 ){
+
+//     alert("welcome")
+//  }
+//  else{
+//     alert("you cant")
+//  }
+
+// }
+// bom()
+
+function bom(){
+
+ var num = prompt("Enter Number");
+ 
+ if( num % 2 == 0 ){
+
+    alert("even number");
+ }
+ else{
+    alert("odd number");
+ }
+
+}
+bom()
+
+function bom(){
+var num = prompt("Enter a number");
+
+if (num > 0) {
+    alert("Number is Positive");
+} 
+else if (num < 0) {
+    alert("Number is Negative");
+} 
+}
+bom() 
+
+// if else 
+    // forloop
+// homework
+
+// 0-33 d
+// 33-60 b
+// 60-100 a
+// 0 se kam hai to enter a correct number
+// 100 se jayda hai to enter  correct number
+// for loop ka use karke 1 se 30 tak number print karne hai loop chala kar
+
+function bom(){
+    var num = prompt("Enter Number")
+
+    if(num >0 && num <33){
+        alert("D")
+    }
+
+    else if(num >=33 && num <60) {
+        alert("B")
+    }
+    else if(num >60 && num <100) {
+        alert("A")
+    }
+    
+}
+
+bom()
