7d45438ad5a35a7a7072d6350edb9aa457892e3a
niteshgoswami72812-create <niteshgoswami72812@gmail.com>
2026-03-18 15:40:34 +0530

    nitesh

---
+++ Dom.js
@@ -0,0 +1,33 @@
+// function nitesh() {
+
+//     let nitesh= document.querySelector('.nitesh')
+
+//     nitesh.innerHTML= "this <i> is </i> bhopal"
+
+//     nitesh.style.color="green"
+    
+// }
+
+function hidetext() {
+
+    let nitesh= document.querySelector('.nitesh')
+
+    nitesh.style.display='none'
+    
+}
+
+function showtext() {
+
+    let nitesh= document.querySelector('.nitesh')
+
+     nitesh.style.display='block'
+
+}
+
+
+
+// homework 
+// hide show karna hai complete
+// counter Program banan haui jiske button
+// increment
+// decrement ye dono rahegi
