ebb5281dfea2e0ba4970d5a0bcdb55c3df3460e7
niteshgoswami72812-create <niteshgoswami72812@gmail.com>
2026-03-19 15:39:12 +0530

    kk

--- Dom.js
+++ Dom.js
@@ -22,13 +22,6 @@ function showtext() {
 
     nitesh.style.display = 'block'
     nitesh.style.color = "red"
-    nitesh.style.backgroundColor = "pink"
-    // nitesh.style.fontWeight= "bold"
-    nitesh.style.fontSize= "50px"
-    //  nitesh.style.position="absolute"
-    //   nitesh.style.top="50px"
-    //    nitesh.style.left="100px"
-    nitesh.style.textAlign = "center"
     nitesh.style.fontFamily = "cursive"
 
 }
@@ -39,21 +32,57 @@ function showtext() {
 let count = 0;
 function increment() {
     count++;
-    document.getElementById("count").innerText = count;
+  let tag=  document.querySelector('#count')
+    tag.innerText = count;
+    tag.style.color="Green"
+    
     
 }
 
-function decrement() {
-    count--;
-    document.getElementById("count").innerText = count;
-}
+// function decrement() {
+//     count--;
+//     let tag= document.querySelector('#count')
+//    tag.innerText = count;
+//    tag.style.color="red"
+// }
 
+
+function decrement() {
+    let tag = document.querySelector('#count');
+    if (count > 0) {
+        count--;
+        tag.innerText = count;
+        tag.style.color = "red";
+    } 
+}
 
 // end counter program
 
+function showImage(){
+
+    let box = document.querySelector('#box');
+    box.innerHTML = '<img src="docotr.PNG" style="width:100%; height:100%;">';
+}
 
 // homework
 // hide show karna hai complete
 // counter Program banan haui jiske button
 // increment
 // decrement ye dono rahegi
+ 
+
+// style in JS 
+
+//  nitesh.style.position="absolute"
+    //   nitesh.style.top="50px"
+    //    nitesh.style.left="100px"
+      // nitesh.style.fontWeight= "bold"
+
+
+      
+
+      // monday test
+    //   array string  methods 
+    //   if else
+    //     promot alert 
+    //   bom dom
