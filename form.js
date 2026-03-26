76c4f3c1004813ba9159b4176b1cc637f079a5c6
niteshgoswami72812-create <niteshgoswami72812@gmail.com>
2026-03-26 15:38:51 +0530

    nitesh

--- form.js
+++ form.js
@@ -18,52 +18,65 @@ let formvalidate = () => {
     let lname = document.querySelector("#namel")
     let number = document.querySelector("#nume")
     let email = document.querySelector("#emaile")
+    let pass = document.querySelector("#passd")
+    let cpass = document.querySelector("#cpassd")
 
     let fnameErr = document.querySelector("#Fnameerr")
     let mnameErr = document.querySelector("#Mnameerr")
     let lnameErr = document.querySelector("#Lnameerr")
     let numErr = document.querySelector("#Enumerr")
     let emailErr = document.querySelector("#Eemailerr")
+    let passerr = document.querySelector("#passerr")
+    let cpasserr = document.querySelector("#cpasserr")
 
     // First Name
-    if (fname.value == "") {
+    if (fname.value.trim() == "") {
         fnameErr.innerHTML = "Enter First Name"
         return false
-    } else {
-        fnameErr.innerHTML = ""
-    }
+  
+     }
 
     // Middle Name
-    if (mname.value == "") {
+    if (mname.value.trim() == "") {
         mnameErr.innerHTML = "Enter Middle Name"
         return false
-    } else {
-        mnameErr.innerHTML = ""
-    }
+    } 
+    
 
     // Last Name
-    if (lname.value == "") {
+    if (lname.value.trim() == "") {
         lnameErr.innerHTML = "Enter Last Name"
         return false
-    } else {
-        lnameErr.innerHTML = ""
     }
 
     // Number
-    if (number.value == "") {
-        numErr.innerHTML = "Enter Number"
+    if (number.value.trim().length!=10) {
+        numErr.innerHTML = "Enter 10 digit Number"
         return false
-    } else {
-        numErr.innerHTML = ""
     }
 
     // Email
-    if (email.value == "") {
-        emailErr.innerHTML = "Enter Email"
+    if (email.value.trim().includes("@") === false || email.value.trim().includes(".com") === false) {
+        emailErr.innerHTML = "Enter Valid Email"
         return false
-    } else {
-        emailErr.innerHTML = ""
+    } 
+
+     // Password 
+    if (pass.value.trim().length<8) {
+        passerr.innerHTML = "Password Must be at least 8 characters long"
+        return false
     }
 
+     // ConfirmPassword 
+    if (cpass.value.trim() != pass.value) {
+        cpasserr.innerHTML = "Please Enter Same Password "
+        return false
+    }
+
+
+     
+
     return true
+
+    
 }
