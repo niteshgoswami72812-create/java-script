e6420ca60eb9fef618ebf90681c314ce6e991f09
niteshgoswami72812-create <niteshgoswami72812@gmail.com>
2026-03-30 15:37:31 +0530

    nitesh

---
+++ Object.js
@@ -0,0 +1,35 @@
+let str= "xyz"
+let details= ["Anurag", 26, "Bhopal", 9753856359]
+
+let mydetails= {
+      myname: "Anuarg",
+    age: 26,
+    city: "Bhopal",
+    number:9753856359
+}
+console.log(mydetails)
+
+
+
+ let btn =()=> {
+    let nitesh =document.querySelector("#text").value.trim()
+
+let mydetails= {
+    myname: nitesh,
+    age: 26,
+    city: "Bhopal",
+    number:9753856359
+}
+console.log(mydetails.myname) 
+ }
+
+ // access
+
+// mydetails.address="Narmadapuram" // insert new value
+
+// mydetails.age="29" // update old value to new value
+
+// // delete mydetails.city // delete old value
+
+// console.log(mydetails);
+
